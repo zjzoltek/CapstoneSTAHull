17c05a9511d6ba1641e5830d9b078a4f3e53cdf1
STASoftDev <162825@student.raytownschools.org>
2016-04-05 13:41:50 -0500

    Started
    I Have some code for drop box for quad

---
+++ CapStoneCA/src/capstoneca/CapStoneCA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstoneca;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author ccallen0504
+ */
+public class CapStoneCA 
+{
+   
+}
---
+++ CapStoneCA/src/capstoneca/PopupTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstoneca;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ccallen0504
+ */
+public class PopupTest {
+    
+    
+    public class void main(String[] args)
+   {
+       PopupFrame popupFrame = new PopupFrame();
+       popupFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       popupFrame.setSize(300, 200);
+       popupFrame.setvisible(true);
+   }
+}
