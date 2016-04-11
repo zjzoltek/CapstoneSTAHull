f2e37ab999035601c37a5e8e21945c4947e10a0e
peb1231 <pebledsoe1231@lsr7.net>
2016-04-11 12:29:41 -0500

    FIxing stuff dw

--- CapstonePB/src/capstonepb/EditPage.java
+++ CapstonePB/src/capstonepb/EditPage.java
@@ -5,8 +5,9 @@
  */
 package capstonepb;
 import java.awt.*;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 
 
@@ -14,19 +15,65 @@ import javax.swing.JFrame;
  *
  * @author pebledsoe1231
  */
-public class EditPage extends JComponent
+public class EditPage extends JPanel
 {
-    int width1;
-    public void editPage (int width)
+    int width;
+    public EditPage(int width)
     {
-        width1 = width;
+        this.width = width;
     }
-    @Override
+    
+    /*@Override
     public void paintComponent(Graphics g)
     {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.DARK_GRAY);
-        g2d.fillRect(0,0,width1,500);//width1);
-    }
+    super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setColor(Color.DARK_GRAY);
+    g2d.fillRect(0,0,width,150);
+    }*/
+    
+   /* public class GridBagLayoutDemo {
+    final static boolean shouldFill = true;
+    final static boolean shouldWeightX = true;
+    final static boolean RIGHT_TO_LEFT = false;
+ 
+    public void addComponentsToPane(Container pane) {
+        if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }}}*/
+    
+    
+    public static void main(String[] args){
+        JFrame frame1 = new JFrame();
+        frame1.setLayout(new GridBagLayout());
+         GridBagConstraints c = new GridBagConstraints();
+    
+        
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        
+        int width = (int)tk.getScreenSize().getWidth();
+        int height = (int)tk.getScreenSize().getHeight();
+        
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        EditPage page = new EditPage(width);
+        frame1.add(page);
+        
+        
+        frame1.setVisible(true);
+        Buttons newContentPane = new Buttons();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame1.setContentPane(newContentPane);
+        frame1.setSize(new Dimension(width, height));
+        //g2d.fillRect(0,0,width, 150);
+        Buttons buttons = new Buttons();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                buttons.createAndShowGUI(); 
+            }
+        });
+        
+   }
 }
