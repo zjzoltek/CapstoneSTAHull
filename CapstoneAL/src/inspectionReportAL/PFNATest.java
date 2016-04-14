14d35d5076a01be479d4c3d3b88c1c294b4cedf4
AustinCLeavitt <acleavitt0612@lsr7.net>
2016-04-14 12:05:43 -0500

    Finished pass fail na class

---
+++ CapstoneAL/src/inspectionReportAL/PFNATest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inspectionReportAL;
+
+/**
+ *
+ * @author ACLeavitt0612
+ */
+
+import javax.swing.JFrame;
+
+public class PFNATest {
+    
+    public static void main(String args[])
+    {
+        JFrame frame = new JFrame();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        PFNA pfna = new PFNA("text");
+        
+        frame.add(pfna);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
