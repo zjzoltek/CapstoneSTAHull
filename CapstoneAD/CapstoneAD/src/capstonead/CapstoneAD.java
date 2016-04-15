f0b0b732bac9526abf0cf39ccdfd18485f502bf2
soccer9721 <awdobson1129@lsr7.net>
2016-04-15 12:39:02 -0500

    expanded the text fields for date, inspectors name and signature. Added the pass fail and not applicable to the top.

--- CapstoneAD/CapstoneAD/src/capstonead/CapstoneAD.java
+++ CapstoneAD/CapstoneAD/src/capstonead/CapstoneAD.java
@@ -17,29 +17,12 @@ public class CapstoneAD{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  
-        /*
-        //String comments = JLabel("Comments: ");
-        String date = JOptionPane.showInputDialog("Date: ");
-        String name = JOptionPane.showInputDialog("What is your name? ");
-        
-        String COMMENTS = String.format("Comments: %s", comments);
-        String DATE = String.format("Date: %s", date);
-        String INSPECTOR = String.format("Inspector: %s", name);
-        
-        JOptionPane.showMessageDialog(null, COMMENTS);
-        JOptionPane.showMessageDialog(null, DATE);
-        JOptionPane.showMessageDialog(null, INSPECTOR);
-        */
-        
-        
         JFrame frame = new JFrame();
-        
-        CommentsInspection panel = new CommentsInspection();
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        CommentsInspection panel = new CommentsInspection(); // new panel
+        frame.add(panel); // print panel
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make it close when you hit the red X in the corner
+        frame.setVisible(true); // make it visible
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // make the screen big
     }
     
 }
--- CapstoneAD/CapstoneAD/src/capstonead/CommentsInspection.java
+++ CapstoneAD/CapstoneAD/src/capstonead/CommentsInspection.java
@@ -16,32 +16,31 @@ import java.awt.*;
 public class CommentsInspection extends JPanel{
         BorderLayout layout = new BorderLayout(); // create layout
         FlowLayout layout2 = new FlowLayout(); // create layout
-        JLabel comments = new JLabel("Comments:"); // create a varriable to hold the string
+        JLabel comments = new JLabel("ADDITIONAL  VIOLATIONS/COMMENTS:                                                                                                                                                                                                                 P = Pass     F = Fail   NA = Not Applicable"); // create a varriable to hold the string
         JLabel date = new JLabel("Date of Report:"); // create a varriable to hold the string
         JLabel inspector = new JLabel("Inspector:"); // create a varriable to hold the string
         JLabel signature = new JLabel("Signature:"); // create a varriable to hold the string
         JTextArea commentArea = new JTextArea(8, 1); // arguements rows, columns
         
-        JTextField dateField = new JTextField(); // create the text fields
-        JTextField inspectorField = new JTextField(); // create the text fields
-        JTextField signatureField = new JTextField(); // create the text fields
+        JTextField dateField = new JTextField(10); // create the text fields and show 10 spaces
+        JTextField inspectorField = new JTextField(10); // create the text fields and show 10 spaces
+        JTextField signatureField = new JTextField(10); // create the text fields and show 10 spaces
         
         JPanel signiturePanel = new JPanel(); // create the text fields
     
         CommentsInspection(){
-
             setLayout(layout); // set the layout
             
             add(comments, BorderLayout.NORTH); // add the string Comments to the screen 
             add(commentArea, BorderLayout.CENTER); // add a text area to the screen
         
-            signiturePanel.setLayout(layout2); // add the string 
-            signiturePanel.add(date); // 
-            signiturePanel.add(dateField); // add the string 
-            signiturePanel.add(inspector);
-            signiturePanel.add(inspectorField); // add the string 
-            signiturePanel.add(signature);
-            signiturePanel.add(signatureField);
+            signiturePanel.setLayout(layout2); // set new layout
+            signiturePanel.add(date); // add the string date of report to the screen
+            signiturePanel.add(dateField); // add a text field to hold the date
+            signiturePanel.add(inspector); // add the string Inspector to the screen
+            signiturePanel.add(inspectorField); // add a text field to hold the inspectors name
+            signiturePanel.add(signature); // add the string Signature to the screen
+            signiturePanel.add(signatureField); // add a text field to hold the signature of who ever signs off on the form
             
             add(signiturePanel, BorderLayout.SOUTH);
         
