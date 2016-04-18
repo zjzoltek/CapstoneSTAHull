e5ff1290a75997a08d54e46cdd4bd1c053e8e347
LHall383 <lahall1009@lsr7.org>
2016-04-18 13:45:09 -0500

    Added More

--- CapstoneLH/src/capstonelh/NumberedField.java
+++ CapstoneLH/src/capstonelh/NumberedField.java
@@ -9,26 +9,39 @@ package capstonelh;
 import inspectionReportAL.PFNA;
 import java.awt.Color;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 public class NumberedField extends JPanel{
+    private BoxLayout layout;
+    
     public NumberedField(int field)
     {
+        layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(layout);
+        
+        
         JLabel title = new JLabel("");
         title.setFont(getFont().deriveFont(16f));
         title.setBorder(BorderFactory.createLineBorder(Color.black));
+//        title.setHorizontalAlignment(SwingConstants.WEST);
         
         JLabel subtitle = new JLabel("P  F  NA");
         subtitle.setFont(getFont().deriveFont(14f));
+//        subtitle.setHorizontalAlignment(SwingConstants.WEST);
         
         
         switch(field)
         {
             case 1:
                 title.setText("   EXIT DOORS/EXIT WAYS   ");
+                
                 PFNA obstruction = new PFNA("Obstruction");
+//                obstruction.setAlignmentX(LEFT_ALIGNMENT);
+                        
                 PFNA locks = new PFNA("Locks/Panic Hardware");
                 
                 //Add objects to the frame
