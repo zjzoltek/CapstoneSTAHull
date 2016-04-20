7d50167a562f6d8f76ce56b3cf73639a31b07378
STASoftDev <162825@student.raytownschools.org>
2016-04-20 13:38:41 -0500

    adding in for the next page

--- CapstoneCA/src/SFLossReduction/sFLRTType.java
+++ CapstoneCA/src/SFLossReduction/sFLRTType.java
@@ -10,8 +10,36 @@ package SFLossReduction;
  * @author ccallen0504
  */
 
-import
+import javax.swing.*;
 
 public class sFLRTType {
     
+    //Title
+    
+    
+    //making the check boxes 
+    // row one
+    private final JCheckBox cB1 = new JCheckBox(" Occupancy ");
+    private final JCheckBox cB2 = new JCheckBox(" Life Safety ");
+    private final JCheckBox cB3 = new JCheckBox(" Sprinkler ");
+    private final JCheckBox cB4 = new JCheckBox(" Hazardous Materials Permint ");
+    //limiting the activity so they can't check both off
+    JRadioButton semi = new JRadioButton("Semi - Annual ");
+    JRadioButton annual = new JRadioButton("Annual");
+    
+    // row two
+    private final JCheckBox cB5 = new JCheckBox(" Complaint ");
+    private final JCheckBox cB6 = new JCheckBox(" Explosive Storage ");
+    private final JCheckBox cB7 = new JCheckBox(" UST ");
+    private final JCheckBox cB8 = new JCheckBox(" Post-Incident ");
+    private final JCheckBox cB9 = new JCheckBox(" Open Burning ");
+    private final JCheckBox cB10 = new JCheckBox(" Other ");
+    
+    JPanel panel = new JPanel();
+    
+    BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.X_AXIS);
+
+    
+    
+           
 }
