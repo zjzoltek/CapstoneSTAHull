1c573d2857856a05852bbb0c26e0760af8d91e1f
AustinCLeavitt <acleavitt0612@lsr7.net>
2016-04-22 12:05:19 -0500

    Fully commented version

--- CapstoneAL/src/inspectionReportAL/Title.java
+++ CapstoneAL/src/inspectionReportAL/Title.java
@@ -1,33 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class uses JLabel to make the title and header for the inspection report form
  */
 package inspectionReportAL;
+
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author ACLeavitt0612
  */
 
-import javax.swing.*;
-import java.awt.*;
+
 
 public class Title extends JPanel{
-    
+    //creates objects for the layout
     private GridBagLayout layout = new GridBagLayout();
     private GridBagConstraints constraints;
-    
+    //creating JLabels
     private JLabel header1 = new JLabel("Lee's Summit, Missouri Fire Department");
     private JLabel header2 = new JLabel("Inspection Report");
     private JLabel header3 = new JLabel("Phone: (816)969-1300");
     
     Title(){
+        //Setting layout and constraints
         setLayout(layout);
         constraints = new GridBagConstraints();
-        
+        //setting font of labels
         header1.setFont(new Font("Serif", Font.BOLD, 20));
         header2.setFont(new Font("Serif", Font.PLAIN,20));
+        //adding components
         addComponent(header1, 0, 0, 1, 2);
         addComponent(header2, 2, 0, 1, 2);
         addComponent(header3, 4, 0, 1, 1);
