db94e3b75c4f567ce85640c530efd806e43a085f
LHall383 <lahall1009@lsr7.org>
2016-04-25 13:31:11 -0500

    Added names to components

---
+++ CapstoneLH/src/capstonelh/FormInfo.java
@@ -0,0 +1,12 @@
+/**
+ * FormInfo: 
+ * @author Lucas Hall
+ * @version Apr 25, 2016
+ */
+
+package capstonelh;
+
+
+public class FormInfo {
+
+}
--- CapstoneLH/src/capstonelh/IRSection.java
+++ CapstoneLH/src/capstonelh/IRSection.java
@@ -116,10 +116,12 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
 
         PFNA obstruction = new PFNA("Obstruction");
+        obstruction.setName("100");
         c.gridy = 2;
         panel.add(obstruction, c);
 
         PFNA locks = new PFNA("Locks/Panic Hardware");
+        locks.setName("101");
         c.gridy = 3;
         panel.add(locks, c);
         
@@ -139,10 +141,12 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA exitSigns = new PFNA("Exit Signs Illuminated");
+        exitSigns.setName("110");
         c.gridy = 2;
         panel.add(exitSigns, c);
         
         PFNA lights = new PFNA("Emer. Lights Working");
+        lights.setName("111");
         c.gridy = 3;
         panel.add(lights, c);
         
@@ -163,14 +167,17 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA maintained = new PFNA("Maintained/Close Fully");
+        maintained.setName("120");
         c.gridy = 2;
         panel.add(maintained, c);
         
         PFNA unapproved = new PFNA("Unapproved Open Devices");
+        unapproved.setName("121");
         c.gridy = 3;
         panel.add(unapproved, c);
         
         PFNA clear = new PFNA("Clear of obstructions");
+        clear.setName("122");
         c.gridy = 4;
         panel.add(clear, c);
         
@@ -190,19 +197,23 @@ public class IRSection extends JPanel{
         c.fill = GridBagConstraints.NONE;
         panel.add(subtitle, c);
         
-        PFNA minimum = new PFNA("Minimum2A10BC");
+        PFNA minimum = new PFNA("Minimum 2A10BC");
+        minimum.setName("130");
         c.gridy = 2;
         panel.add(minimum, c);
         
         PFNA maximum = new PFNA("Max of 75' travel dist.");
+        maximum.setName("131");
         c.gridy = 3;
         panel.add(maximum, c);
         
         PFNA inspection = new PFNA("Annual Inspection");
+        inspection.setName("132");
         c.gridy = 4;
         panel.add(inspection, c);
         
         PFNA access = new PFNA("Access");
+        access.setName("133");
         c.gridy = 5;
         panel.add(access, c);
         
@@ -223,22 +234,27 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA extCords = new PFNA("Extension Cords");
+        extCords.setName("140");
         c.gridy = 2;
         panel.add(extCords, c);
         
         PFNA adapters = new PFNA("Multi Plug Adapters");
+        adapters.setName("141");
         c.gridy = 3;
         panel.add(adapters, c);
         
         PFNA boxes = new PFNA("Open Boxes, Switches");
+        boxes.setName("142");
         c.gridy = 4;
         panel.add(boxes, c);
         
         PFNA electricalPanel = new PFNA("Openings in Electrical Panel");
+        electricalPanel.setName("143");
         c.gridy = 5;
         panel.add(electricalPanel, c);
         
         PFNA clearance = new PFNA("36\" clearance from storage");
+        clearance.setName("144");
         c.gridy = 6;
         panel.add(clearance, c);
         
@@ -259,22 +275,27 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA housekeeping = new PFNA("Housekeeping");
+        housekeeping.setName("150");
         c.gridy = 2;
         panel.add(housekeeping, c);
         
         PFNA ceiling = new PFNA("24\" below ceiling");
+        ceiling.setName("151");
         c.gridy = 3;
         panel.add(ceiling, c);
         
         PFNA sprinklers = new PFNA("18\" below sprinklers");
+        sprinklers.setName("152");
         c.gridy = 4;
         panel.add(sprinklers, c);
         
         PFNA stairs = new PFNA("Uner stairs");
+        stairs.setName("153");
         c.gridy = 5;
         panel.add(stairs, c);
         
         PFNA ignition = new PFNA("Close to ignition source");
+        ignition.setName("154");
         c.gridy = 6;
         panel.add(ignition, c);
         
@@ -295,14 +316,17 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA placards = new PFNA("704 Placards");
+        placards.setName("160");
         c.gridy = 2;
         panel.add(placards, c);
         
         PFNA msds = new PFNA("MSDS on premises");
+        msds.setName("161");
         c.gridy = 3;
         panel.add(msds, c);
         
         PFNA signs = new PFNA("Non-smoking signs");
+        signs.setName("162");
         c.gridy = 4;
         panel.add(signs, c);
         
@@ -323,6 +347,7 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA secured = new PFNA("Secured");
+        secured.setName("170");
         c.gridy = 2;
         panel.add(secured, c);
         
@@ -343,18 +368,22 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA extinguisher = new PFNA("Minimum 4A40BC fire extinguisher within 30'");
+        extinguisher.setName("180");
         c.gridy = 2;
         panel.add(extinguisher, c);
         
         PFNA sprayBooth = new PFNA("Approved spray booth");
+        sprayBooth.setName("181");
         c.gridy = 3;
         panel.add(sprayBooth, c);
         
         PFNA autoExt = new PFNA("Auto extinguishing system Inspected Annually");
+        autoExt.setName("182");
         c.gridy = 4;
         panel.add(autoExt, c);
         
         PFNA warningSigns = new PFNA("Welding/hotwork warning signs and No Smoking signs");
+        warningSigns.setName("183");
         c.gridy = 5;
         panel.add(warningSigns, c);
         
@@ -380,26 +409,30 @@ public class IRSection extends JPanel{
         
         c.gridy = 2;
         PFNA service = new PFNA("Annual Service");
+        service.setName("190");
         panel.add(service, c);
         
         c.gridy = 3;
-        //date of last service here
+        dateField.setName("191");
         panel.add(dateField, c);
         
         c.gridy = 4;
         PFNA access = new PFNA("Access");
+        access.setName("192");
         panel.add(access, c);
         
         PFNA standpipe = new PFNA("Standpipe sticker on door");
+        standpipe.setName("193");
         c.gridy = 5;
         panel.add(standpipe, c);
         
         PFNA sprinklerRoom = new PFNA("Sprinkler room identified");
+        sprinklerRoom.setName("194");
         c.gridy = 6;
         panel.add(sprinklerRoom, c);
         
-        //riser location here
         c.gridy = 7;
+        riserLocation.setName("195");
         panel.add(riserLocation, c);
         
         add(panel, BorderLayout.CENTER);
@@ -421,30 +454,36 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA service = new PFNA("Service within 6 months");
+        service.setName("200");
         c.gridy = 2;
         panel.add(service, c);
         
-        //last service date here
         c.gridy = 3;
+        lastService.setName("201");
         panel.add(lastService, c);
         
         PFNA access = new PFNA("Access to remote pull");
+        access.setName("202");
         c.gridy = 4;
         panel.add(access, c);
         
         PFNA clean = new PFNA("Clean");
+        clean.setName("203");
         c.gridy = 5;
         panel.add(clean, c);
         
         PFNA extinguisher = new PFNA("\"K\" Class extinguisher");
+        extinguisher.setName("204");
         c.gridy = 6;
         panel.add(extinguisher, c);
         
         PFNA nozzle = new PFNA("Nozzle Condition");
+        nozzle.setName("205");
         c.gridy = 7;
         panel.add(nozzle, c);
         
         PFNA filters = new PFNA("Filters in place");
+        filters.setName("206");
         c.gridy = 8;
         panel.add(filters, c);
         
@@ -465,6 +504,7 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA posted = new PFNA("Posted in places of assembly");
+        posted.setName("210");
         c.gridy = 2;
         panel.add(posted, c);
         
@@ -485,6 +525,7 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA visible = new PFNA("Visible & legible");
+        visible.setName("220");
         c.gridy = 2;
         panel.add(visible, c);
         
@@ -507,14 +548,16 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA maint = new PFNA("Maint/Lubrication");
+        maint.setName("230");
         c.gridy = 2;
         panel.add(maint, c);
         
         PFNA boxKeys = new PFNA("Knox box keys current");
+        boxKeys.setName("231");
         c.gridy = 3;
         panel.add(boxKeys, c);
         
-        //Knox box location here
+        knox.setName("232");
         c.gridy = 5;
         panel.add(knox, c);
         
@@ -537,18 +580,21 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA caps = new PFNA("FDC caps in place");
+        caps.setName("240");
         c.gridy = 2;
         panel.add(caps, c);
         
         PFNA identified = new PFNA("FDC identified");
+        identified.setName("241");
         c.gridy = 3;
         panel.add(identified, c);
         
         PFNA access = new PFNA("FDC access is unobstructed");
+        access.setName("242");
         c.gridy = 4;
         panel.add(access, c);
         
-        //FDC location here
+        fdc.setName("243");
         c.gridy = 5;
         panel.add(fdc, c);
         
@@ -569,6 +615,7 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA keys = new PFNA("Override Keys On-Site");
+        keys.setName("250");
         c.gridy = 2;
         panel.add(keys, c);
         
@@ -590,14 +637,17 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         PFNA placards = new PFNA("704 Placards");
+        placards.setName("260");
         c.gridy = 2;
         panel.add(placards, c);
         
         PFNA permit = new PFNA("Permit on site");
+        permit.setName("261");
         c.gridy = 3;
         panel.add(permit, c);
         
         PFNA bollards = new PFNA("Impact bollards");
+        bollards.setName("262");
         c.gridy = 4;
         panel.add(bollards, c);
         
@@ -618,6 +668,7 @@ public class IRSection extends JPanel{
         panel.add(subtitle, c);
         
         JCheckBox button = new JCheckBox("Other Violations Noted - Use box below", false);
+        button.setName("270");
         c.gridy = 4;
         panel.add(button, c);
         
